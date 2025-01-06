4a7b8ec3b61bc898e290687f0382dca07e656197
Sudeep <sudeep7217@gmail.com>
2025-01-07 01:15:11 +0530

    Add Vercel Speed Insights integration to layout and update package.json

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Orbitron, Poppins, Raleway } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const orbitron = Orbitron({
   subsets: ["latin"],
@@ -36,6 +37,7 @@ export default function RootLayout({ children }) {
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
